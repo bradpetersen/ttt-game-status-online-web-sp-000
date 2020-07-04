2bab64f72582d8ac8e356dff62b39e3c853f9a07
Brad Petersen <brad.petersen7@gmail.com>
2020-07-04 08:57:09 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,9 +6,9 @@ end
 # Define your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [
-[0, 1, 2]
-[3, 4, 5]
-[6, 7, 8]
-[0, 4, 8]
-[2, 4, 6]
+[0, 1, 2] # top row
+[3, 4, 5] # middle row
+[6, 7, 8] # bottom row
+[0, 4, 8] # \ cross
+[2, 4, 6] # / cross
 ]
